b0e0e0fbf54d958abcebbf764ecf8473e8091244
Dev Prakash <68344365+devparkk01@users.noreply.github.com>
2021-09-07 17:10:23 +0530

    empty dependency array

--- src/index.js
+++ src/index.js
@@ -1,56 +1,52 @@
 import ReactDOM from "react-dom";
-import React , {useState , useEffect } from "react" ;
+import React, { useState, useEffect } from "react";
 
-
-// here , effect will be called only when props.name changes .
-// if props.num change then effect won't be called , only the component will 
-// be rendered
+// here , empty dependency array is passed .
+// useEffect will be called only once after initial rendering
+// useEffect will be never called now ,only the component will be re-rendered
 
 const Show = (props) => {
-  useEffect( () => {
-    console.log("useEffect called on " + props.name ) ;
+  useEffect(() => {
+    console.log("useEffect called on " + props.name);
 
     return () => {
-      console.log("clean up called on " + props.name ) ; 
-    }
-  }, [props.name] )
-
+      console.log("clean up called on " + props.name);
+    };
+  }, []);
 
   return (
     <div>
-       { console.log("rendered " + props.name ) }
-       <h1> rendered : {props.name} </h1>
-    </div> 
-  )
-}
-
+      {console.log("rendered " + props.name)}
+      <h1> rendered : {props.name} </h1>
+    </div>
+  );
+};
 
 function Counter() {
   const [value, setValue] = useState("Empty");
-  const [num , setNum ] = useState(0) ; 
+  const [num, setNum] = useState(0);
 
   // click handler for first two buttons
-  const clickHandler = (event ) => {
-    setValue(event.target.innerText) ; 
-  }
+  const clickHandler = (event) => {
+    setValue(event.target.innerText);
+  };
   // click handler for last two buttons
   const secondClickHandler = (event) => {
-    setNum(event.target.innerText) ; 
-  } 
-
+    setNum(event.target.innerText);
+  };
 
   return (
-    <div > 
-      <button onClick = { clickHandler } > pizza </button>
-      <button onClick = { clickHandler } > Burger </button>
-      <button onClick = { secondClickHandler } > 10 </button >
-      <button onClick = { secondClickHandler } > 20 </button >
-      <Show name = {value} num = {num}> </Show>
-
+    <div>
+      <button onClick={clickHandler}> pizza </button>
+      <button onClick={clickHandler}> Burger </button>
+      <button onClick={secondClickHandler}> 10 </button>
+      <button onClick={secondClickHandler}> 20 </button>
+      <Show name={value} num={num}>
+        {" "}
+      </Show>
     </div>
-  )
+  );
 }
 
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(  <Counter />  ,rootElement );
+ReactDOM.render(<Counter />, rootElement);
